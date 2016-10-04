68f4b4211687c26d5743685e8a7762f5cb139e4f
gk kirchner <gk kirchner>
2016-10-03 20:41:34 -0700

    correcting server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.use(function(req, res, next){
 	if(req.headers['x-forwarded-proto'] === 'http'){
 		next();
 	}else{
-		response.redirect('http://' + req.hostname + req.url);
+		res.redirect('http://' + req.hostname + req.url);
 	}
 });
 app.use(express.static('public'));
 
 app.listen(PORT, function(){
 	console.log("express server is up on port " + PORT);
-})
+});
