828d71bdd0b181fbe9f3bfbd981a49be0e0d93c4
gk kirchner <gk kirchner>
2016-10-05 12:22:23 -0700

    style navbar

--- app/components/nav.jsx
+++ app/components/nav.jsx
@@ -16,15 +16,36 @@ var Nav = React.createClass({
 })
 
 */                                                                                                                                    
-var Nav = ()=>{
-	return(
-			<div>
-			<h1>navbar</h1>
-			<IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-			<Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-			<Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-			</div>
-		);
-}
+var Nav = React.createClass({
+	onSearch: function(e){
+		e.preventDefault();
+		alert("search works");
+	},
+
+	render: function(){
+		return(
+		<div className = "top-bar">
+				<div className = "top-bar-left">
+					<ul className = "dropdown menu" data-dropdown-menu>
+					<li className = "menu-text">React Weather App</li>
+						<li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink></li>
+						<li><Link to="/about" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link></li>
+						<li><Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link></li>
+					</ul>
+				</div>
+				<div className = "top-bar-right">
+					<form onSubmit = {this.onSearch}>
+					<ul className = "menu">
+      					<li><input type="search" placeholder="Search weather"/></li>
+      					<li><input type="submit" className="button" value = "Get Weather"/></li>
+      				</ul>
+      				</form>
+				</div>
+		</div>
+	);
+	}
+	
+})
 
 module.exports = Nav;
+
